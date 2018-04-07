f104ff027dc09f29c51a00a8e0ec1ccafbff3dde
markwicked <markwicked@gmail.com>
2018-04-07 17:25:53 +0500

    123

--- Sedona/js/popup.js
+++
@@ -1,7 +0,0 @@
-var link = document.querySelector(".modal-search-button");
-var popup = document.querySelector(".search-form");
-
-
-link.addEventListener("click", function(evt) {
-  popup.classList.toggle("search-form-show");
-});
